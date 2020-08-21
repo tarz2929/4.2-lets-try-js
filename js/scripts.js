e139a826f4ab93b2df0744b587090f7155ccb155
tarz2929 <saitarunmulley@gmail.com>
2020-08-21 10:28:04 -0600

    added some basic stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,41 @@
-alert("Hello, World! JavaSript is loaded");
+// alert("Hello, World! JavaSript is loaded");
+
+/*
+* Assignments/Variables
+*/
+
+
+var myFirstvariable = "Hello"
+
+// 
+let mySecondVariable = "World";
+
+//
+const myThirdvariable = myFirstvariable + "," + mySecondVariable;
+// Concatenation character is the "+" sign.
+// it glues the string togerther
+/*
+* Datatypes
+*/
+
+const myString = "This is a string!";
+const myInteger = 34; //Ints and Floats are technically the same data tyoe in JS. 
+const myFloat = 16.782;
+const myNan = NaN; //not a number" So when math goes horribly wrong..
+const myTrueBool = true; // Booleans can either be True or False
+const myFalseBool = false;
+const myNull = null; // A defined / existing variable with no assigned value. 
+const myUndefined = undefined; 
+const myArra = [
+    myString,
+    myInteger,
+    myFloat,
+    myNan,
+    myTrueBool,
+    myFalseBool,
+    myNull,
+    myUndefined
+];
+
+
+
